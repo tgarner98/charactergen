902995e2d7d0749c823e5d8df472515803a8e3a1
tgarner98 <68405466+tgarner98@users.noreply.github.com>
2020-07-27 22:45:44 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,552 @@
+// Races and Names //
+
+// Male Names //
+var male = new Array();
+male[0] = new Array( // Aarakocra //
+    "Rek",
+    "Khil",
+    "Crilag",
+    "Zed",
+    "Qhed",
+    "Rhiss",
+    "Keck",
+    "Ahk",
+    "Kharrk",
+    "Dirk"
+);
+male[1] = new Array( // Dragonborn //
+    "Gompok",
+    "Krakath",
+    "Naqull",
+    "Uthtir",
+    "Certhar",
+    "Drastis",
+    "Kildar",
+    "Myulmin",
+    "Tazbroth",
+    "Balzire"
+);
+male[2] = new Array( // Drow //
+    "Yazlan",
+    "Jivrak",
+    "Itzar",
+    "Nyrmil",
+    "Vielin",
+    "Nador",
+    "Melakiir",
+    "Nalfein",
+    "Kodar",
+    "Selakiir"
+);
+male[3] = new Array( // Dwarf //
+    "Grahldor",
+    "Tornik",
+    "Bertharn",
+    "Harmiir",
+    "Balmyr",
+    "Daermek",
+    "Dalgron",
+    "Therdor",
+    "Banrik",
+    "Galron"
+);
+male[4] = new Array( // Eldarin //
+    "Jathas",
+    "Panthas",
+    "Yanmil",
+    "Dageon",
+    "Zoremin",
+    "Vicrian",
+    "Uridan",
+    "Corbaver",
+    "Brantorin",
+    "Tavkul"
+);
+male[5] = new Array( // Elf //
+    "Zinven",
+    "Herfir",
+    "Sylneiros",
+    "Yinro",
+    "Iliris",
+    "Naelar",
+    "Luzeiros",
+    "Genmaris",
+    "Virris",
+    "Thedan"    
+);
+male[6] = new Array( // Gnome //
+    "Zanlen",
+    "Bilhim",
+    "Raszu",
+    "Nesston",
+    "Tanvyn",
+    "Calkas",
+    "Keltor",
+    "Farver",
+    "Wilkas",
+    "Ronji"    
+);
+male[7] = new Array( // Half-Elf //
+    "Wilorin",
+    "Belvalor",
+    "Vanelor",
+    "Osovar",
+    "Yenros",
+    "Tyrkoris",
+    "Nilhorn",
+    "Raffyr",
+    "Vanorin",
+    "Zylorin"    
+);
+male[8] = new Array( // Halfling //
+    "Merlos",
+    "Davmin",
+    "Arlos",
+    "Elzu",
+    "Horzu",
+    "Korzin",
+    "Kasdak",
+    "Zalrich",
+    "Sharbin",
+    "Pimdal"    
+);
+male[9] = new Array( // Human //
+    "Rashin",
+    "Fusim",
+    "Rirden",
+    "Shordar",
+    "Garth",
+    "Alun",
+    "Zerder",
+    "Hivif",
+    "Dakren",
+    "Kordim"    
+);
+male[10] = new Array( // Lizardfolk //
+    "Tiltotu",
+    "Kolrur",
+    "Leslurk",
+    "Girisk",
+    "Dardor",
+    "Nurko",
+    "Sirkur",
+    "Nech",
+    "Jhilit",
+    "Lorkit"    
+);
+male[11] = new Array( // Orc //
+    "Bharzug",
+    "Zhobar",
+    "Ghok",
+    "Ghog",
+    "Nubron",
+    "Lanakk",
+    "Grubzol",
+    "Ghavol",
+    "Rabzokk",
+    "Zhag"    
+);
+male[12] = new Array( // Tabaxi //
+    "Hide",
+    "Fang",
+    "Quill",
+    "Dew",
+    "Scratch",
+    "Lock",
+    "Rock",
+    "Stitch",
+    "Gale",
+    "Robin"   
+);
+male[13] = new Array( // Tiefling //
+    "Skavius",
+    "Valrus",
+    "Mavilius",
+    "Shathos",
+    "Xarrus",
+    "Malus",
+    "Skamos",
+    "Salreus",
+    "Zarmir",
+    "Revir"    
+);
+
+
+
+// Female Names //
+
+var female = new Array();
+female[0] = new Array( // Aarakocra //
+    "Kha",
+    "Rheca",
+    "Arra",
+    "Zuki",
+    "Rhe",
+    "Girri",
+    "Daeqa",
+    "Arri",
+    "Su",
+    "Ruie"
+);
+female[1] = new Array( // Dragonborn //
+    "Thameila",
+    "Abis",
+    "Dister",
+    "Pralkun",
+    "Vyurinn",
+    "Faewyn",
+    "Zarinn",
+    "Tuaxis",
+    "Firuuc",
+    "Irnoc"
+);
+female[2] = new Array( // Drow //
+    "Quarri",
+    "Faetyn",
+    "Lyra",
+    "Ilmra",
+    "Yasala",
+    "Zendaza",
+    "Felynnia",
+    "Halxae",
+    "Vierva",
+    "Ereldra"
+);
+female[3] = new Array( // Dwarf //
+    "Nalvia",
+    "Mystin",
+    "Gymryn",
+    "Karnis",
+    "Runvia",
+    "Edma",
+    "Lyssri",
+    "Edlyn",
+    "Gwennis",
+    "Lesgiel"
+);
+female[4] = new Array( // ELdarin //
+    "Qixis",
+    "Faehyssa",
+    "Faegrys",
+    "Savyre",
+    "Velameiv",
+    "Magqis",
+    "Alcena",
+    "Sameiv",
+    "Ulvara",
+    "Birxana"
+);
+female[5] = new Array( // Elf //
+    "Sylthyra",
+    "Inara",
+    "Reydi",
+    "Valdove",
+    "Sylkrana",
+    "Sylleth",
+    "Bira",
+    "Shadi",
+    "Syldi",
+    "Dayra"
+);
+female[6] = new Array( // Gnome //
+    "Faenoa",
+    "Fentra",
+    "Eiliza",
+    "Nysys",
+    "Talys",
+    "Zintra",
+    "Jobi",
+    "Tabi",
+    "Nihani",
+    "Nynove"
+);
+female[7] = new Array( // Half-Elf //
+    "Sylphira",
+    "Jenys",
+    "Resnoa",
+    "Zelstine",
+    "Kyszira",
+    "Reldiane",
+    "Alystine",
+    "Qidove",
+    "Elgwynn",
+    "Faeona"    
+);
+female[8] = new Array( // Halfling //
+    "Hala",
+    "Nedalyn",
+    "Paezira",
+    "Kithna",
+    "Froyra",
+    "Verni",
+    "Fayni",
+    "Diari",
+    "Vawyn",
+    "Nevira"    
+);
+female[9] = new Array( // Human //
+    "Nalni",
+    "Nihne",
+    "Recha",
+    "Tsui",
+    "Evri",
+    "Lithra",
+    "Kefla",
+    "Miao",
+    "Cephani",
+    "Kari"    
+);
+female[10] = new Array( // Lizardfolk //
+    "Vegy",
+    "Atega",
+    "Rerdy",
+    "Lipekri",
+    "Ari",
+    "Ishe",
+    "Nagge",
+    "Tri",
+    "Va",
+    "Thirga"    
+);
+female[11] = new Array( // Orc //
+    "Kuansu",
+    "Ghulla",
+    "Bounsaa",
+    "Migvo",
+    "Illi",
+    "Uwna",
+    "Showni",
+    "Edgi",
+    "Ghanza",
+    "Bedduk"    
+);
+female[12] = new Array( // Tabaxi //
+    "Sapphire",
+    "Bubble",
+    "Dream",
+    "Blossom",
+    "Misty",
+    "Night",
+    "Summer",
+    "River",
+    "Angel",
+    "Sky"
+);
+female[13] = new Array( // Tiefling //
+    "Belki",
+    "Livari",
+    "Yuhiri",
+    "Griyola",
+    "Seiriza",
+    "Riki",
+    "Satari",
+    "Nayis",
+    "Roza",
+    "Nithari",
+    "Phoebe"
+);
+
+var charclass = new Array(
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard"
+);
+
+
+
+
+// Character Generator //
+function genChar() {
+    var gender = document.getElementsByName("gender");
+    for (i = 0; i < gender.length; i++){ // Name Generator //
+        if (gender[i].checked){
+            document.getElementById("givenName").innerHTML = gender[i].value;
+            var race = document.getElementById("race").value;
+            if (gender[0].checked){
+                var ran = Math.floor(Math.random() * ((male[race].length)-0) + 0); // Generates number between 0 and length of array to randomly select name. Can add names freely instead of editing max number.
+                document.getElementById("givenName").innerHTML = male[race][ran]; // Writes selected name to HTML
+            } else {
+                var ran = Math.floor(Math.random() * ((female[race].length)-0) + 0);
+                document.getElementById("givenName").innerHTML = female[race][ran];
+            };
+        };
+    };
+    var str;
+    var dex;
+    var int;
+    var wis;
+    var cha;
+    var con;
+
+    var charClass = document.getElementById("charClass").value;
+    var level = document.getElementById("charLevel").value;
+    var rolls = [ // Makes six rolls between 10 and 18 //
+        Math.floor(Math.random() * (19-10) + 10),
+        Math.floor(Math.random() * (19-10) + 10),
+        Math.floor(Math.random() * (19-10) + 10),
+        Math.floor(Math.random() * (19-10) + 10),
+        Math.floor(Math.random() * (19-10) + 10),
+        Math.floor(Math.random() * (19-10) + 10) 
+    ];
+
+    // Stat Generator //
+
+    rolls.sort();
+
+    // Distributes rolls array values to stat variables based on selected character class
+
+    // Barbarian //
+    if (charClass == 0) {
+        str = rolls[5];
+        con = rolls[4];
+        dex = rolls[3];
+        cha = rolls[2];
+        wis = rolls[1];
+        int = rolls[0];
+
+
+        // Bard //
+    } else if (charClass == 1) {
+        cha = rolls[5];
+        dex = rolls[4];
+        int = rolls[3];
+        wis = rolls[2];
+        con = rolls[1];
+        str = rolls[0];
+
+        // Cleric //
+    } else if (charClass == 2) {
+        wis = rolls[5];
+        cha = rolls[4];
+        int = rolls[3];
+        con = rolls[2];
+        dex = rolls[1];
+        str = rolls[0];
+
+        // Druid //
+    } else if (charClass == 3) {
+        wis = rolls[5];
+        int = rolls[4];
+        cha = rolls[3];
+        con = rolls[2];
+        dex = rolls[1];
+        str = rolls[0];
+
+        // Fighter //
+    } else if (charClass == 4) {
+        str = rolls[5];
+        dex = rolls[4];
+        con = rolls[3];
+        cha = rolls[2];
+        wis = rolls[1];
+        int = rolls[0];
+
+        // Monk //
+    } else if (charClass == 5) {
+        dex = rolls[5];
+        wis = rolls[4];
+        int = rolls[3];
+        cha = rolls[2];
+        str = rolls[1];
+        con = rolls[0];
+
+        // Paladin //
+    } else if (charClass == 6) {
+        str = rolls[5];
+        cha = rolls[4];
+        con = rolls[3];
+        dex = rolls[2];
+        wis = rolls[1];
+        int = rolls[0];
+
+        // Ranger //
+    } else if (charClass == 7) {
+        dex = rolls[5];
+        wis = rolls[4];
+        int = rolls[3];
+        cha = rolls[2];
+        con = rolls[1];
+        str = rolls[0];
+
+        // Rogue //
+    } else if (charClass == 8) {
+        dex = rolls[5];
+        int = rolls[4];
+        cha = rolls[3];
+        wis = rolls[2];
+        str = rolls[1];
+        con = rolls[0];
+
+        // Sorcerer //
+    } else if (charClass == 9) {
+        cha = rolls[5];
+        con = rolls[4];
+        int = rolls[3];
+        wis = rolls[2];
+        dex = rolls[1];
+        str = rolls[0];
+
+        // Warlock //
+    } else if (charClass == 10) {
+        cha = rolls[5];
+        wis = rolls[4];
+        int = rolls[3];
+        con = rolls[2];
+        dex = rolls[1];
+        str = rolls[0];
+
+        // Wizard //
+    } else {
+        int = rolls[5];
+        wis = rolls[4];
+        con = rolls[3];
+        cha = rolls[2];
+        dex = rolls[1];
+        str = rolls[0];
+    };
+
+    // Write final stat calculations to DOM
+    document.getElementById("givenStrength").innerText = "Strength: " + str;
+    document.getElementById("givenIntelligence").innerText = "Intelligence: " + int;
+    document.getElementById("givenWisdom").innerText = "Wisdom: " + wis;
+    document.getElementById("givenDexterity").innerText = "Dexterity: " + dex;
+    document.getElementById("givenConstitution").innerText = "Constitution: " + con;
+    document.getElementById("givenCharisma").innerText = "Charisma: " + cha;
+
+    
+
+    // Skills //
+    var skillMods = [
+        0, // 10-11
+        1, // 12-13
+        2, // 14-15
+        3, // 16-17
+        4, // 18-19
+        5 // 20-21
+    ];
+
+    if (str == 10 || str == 11 ) {
+        str = skillMods[0];
+    } else if (str == 12 || str == 13) {
+        str = skillMods[1];
+    } else if (str == 14 || str == 15) {
+        str = skillMods[2];
+    } else if (str == 16 || str == 17) {
+        str = skillMods[3];
+    } else if (str == 18 || str == 19) {
+        str = skillMods[4];
+    } else {
+        str = skillMods[5];
+    };
+    console.log("Stat Mod: " + str);
+    
+};
